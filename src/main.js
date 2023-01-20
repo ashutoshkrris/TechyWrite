5ab5a2e6facae15c66dd031b3ea970696055dfb5
Ashutosh Krishna <ashutoshbritish@gmail.com>
2023-01-20 16:27:22 +0530

    Added Bootstrap Icons

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
-import DefaultLayout from '~/layouts/Default.vue'
-import "~/assets/style.css"
+import DefaultLayout from "~/layouts/Default.vue";
+import "~/assets/style.css";
 
-export default function (Vue, { router, head, isClient }) {
+export default function(Vue, { router, head, isClient }) {
   // Set default layout as a global component
-  Vue.component('Layout', DefaultLayout)
+  Vue.component("Layout", DefaultLayout);
+
+  // Add an external CSS file
+  head.link.push({
+    rel: "stylesheet",
+    href:
+      "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css",
+  });
 }
